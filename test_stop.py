eb329c095e9e5618dd4c68ac6e84f04846cf29bf
tianlin shi <tianlinshi@gmail.com>
2014-08-08 02:33:05 +0800

    .

--- test_stop.py
+++ test_stop.py
@@ -1,8 +1,8 @@
 import numpy as np
 import os, sys
 import numpy.random as npr
-import matplotlib.pyplot as plt
-from stat_stop import *
+# import matplotlib.pyplot as plt
+# from stat_stop import *
 
 c_l = [0.001, 0.01, 0.03, 0.07, 0.1, 0.3, 1]
 T_l = [0, 1, 2, 3, 4, 5]
@@ -12,21 +12,17 @@ if len(sys.argv[1]) < 2:
 
 if sys.argv[1] == "toy":
   for c in c_l:
-    cmd = '''./stop --inference Gibbs --T 5 --K 3 --B 0 --eta 0.01 \
- --c %f --name gibbs_c%f --numThreads 3 --trainCount 1000 \
---testCount 1000''' % (c, c)
+    cmd = '''./stop --inference Gibbs --T 5 --K 10 --B 0 --eta 0.01 \
+ --c %f --name gibbs_c%f --numThreads 10 --trainCount -1 \
+--testCount -1 --train data/wsj/wsj-pos.train --test data/wsj/wsj-pos.test''' % (c, c)
     print cmd
     os.system(cmd)  
-    stop = StopResult('gibbs_c%f' % c)
-    print 'c = ', c, 'acc = ', stop.accuracy
 elif sys.argv[1] == "toy0":
   for T in T_l:
-    cmd = '''./stop --inference Gibbs --T %d --name gibbs0_T%d --numThreads 3 \
-    --trainCount 1000 --testCount 1000 --adaptive false''' % (T, T)
+    cmd = '''./stop --inference Gibbs --T %d --name gibbs0_T%d --numThreads 10 \
+    --trainCount -1 --testCount -1 --adaptive false --train data/wsj/wsj-pos.train --test data/wsj/wsj-pos.test''' % (T, T)
     print cmd
     os.system(cmd)  
-    stop = StopResult('gibbs0_T%d' % T)
-    print 'T = ', T, 'acc = ', stop.accuracy
 elif sys.argv[1] == 'plot_toy':
     time = list()
     acc = list()

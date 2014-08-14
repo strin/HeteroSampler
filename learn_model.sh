ec8d65f80d1cd4d12d5d2883eda38004b55ecf69
Strin <tianlinshi@gmail.com>
2014-08-14 15:30:45 +0800

    launch script for Czech

--- learn_model.sh
+++ learn_model.sh
@@ -1,9 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 # learn model using ./pos, $1: task, $2: inference method.
 if [ $1 == "POS_NER" ]; then 
-  cmd="./pos --inference $2 --T 10 --B 3 --train data/eng_pos_ner/train --test data/eng_pos_ner/test --eta 1 --windowL 2 \
-    --output model/ner_pos_gibbs.model --scoring Acc "
-  echo $cmd
-  ($cmd) > ner_pos_gibbs.xml
+  for windowL in `seq 0 2` 
+  do
+    cmd="./pos --inference $2 --T 10 --B 3 --train data/eng_pos_ner/train --test data/eng_pos_ner/test --eta 1 --windowL "$windowL" \
+      --output model/ner_pos_gibbs_w"$windowL".model --scoring Acc --Q 2"
+    echo $cmd
+    ($cmd) > pos_ner_gibbs_w$windowL.xml &
+  done
+elif [ $1 == "Czech" ]; then 
+  for windowL in `seq 0 2`
+  do
+    cmd="./pos --inference $2 --T 10 --B 3 --train data/czech_ner/train --test data/czech_ner/test --eta 1 --windowL "$windowL" \
+      --output model/ner_pos_gibbs_w"$windowL".model --scoring NER --Q 3 "
+    echo $cmd
+    ($cmd) > czech_gibbs_$windowL.xml &
+  done
 fi
 

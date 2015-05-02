9a0b2e458b8e8c801a3c74997f2c607370cd5e48
strin <stl501@gmail.com>
2015-05-02 20:09:23 +0800

    fix bug

--- Makefile.sh
+++ Makefile.sh
@@ -1,7 +1,18 @@
+run () {
+  echo $1
+  $1
+}
+
 cd scilog
-g++ --shared -o libscilog.dylib src/*.cpp -I./inc
-g++ -o testlog testlog.cpp -L./ -lscilog -I./inc
+COMPILE_SCILOG="g++ -std=c++11 --shared -o libscilog.so src/*.cpp -I./inc -fPIC"
+LINK_SCILOG="g++ -std=c++11 -o testlog testlog.cpp -L./ -lscilog -I./inc"
+run "$COMPILE_SCILOG"
+run "$LINK_SCILOG"
 cd ..
-g++ --shared -o libheterosampler.so -std=c++11 -Wno-deprecated-register -Wno-overloaded-virtual src/*.cpp -Iinc/ -Iscilog/inc/ -Lscilog/ -lscilog -lboost_program_options
-g++ -std=c++11 -Wno-deprecated-register -Wno-overloaded-virtual -o bin/tagging model_tagging.cpp src/*.cpp -Iinc/ -Iscilog/inc/ -Lscilog/ -lscilog -lboost_program_options
-g++ -std=c++11 -Wno-deprecated-register -Wno-overloaded-virtual test_policy.cpp -L./ -lheterosampler -Iinc/ -Iscilog/inc/ -Lscilog/ -lscilog -lboost_program_options -o bin/policy -lhdf5
+COMPILE_HS="g++ --shared -o libheterosampler.so -std=c++11 -Wno-deprecated-register -Wno-overloaded-virtual src/*.cpp
+-Iinc/ -Iscilog/inc -Iscilog/inc/ -Lscilog/ -lscilog -lboost_program_options -fPIC"
+COMPILE_TAGGING="g++ -std=c++11 -Wno-deprecated-register -Wno-overloaded-virtual model_tagging.cpp -L./ -lheterosampler -Iinc/ -Iscilog/inc/ -Lscilog/ -lscilog -lboost_program_options -o bin/tagging"
+COMPILE_POLICY="g++ -std=c++11 -Wno-deprecated-register -Wno-overloaded-virtual test_policy.cpp -L./ -lheterosampler -Iinc/ -Iscilog/inc/ -Lscilog/ -lscilog -lboost_program_options -o bin/policy -lhdf5 "
+run "$COMPILE_HS"
+run "$COMPILE_TAGGING"
+run "$COMPILE_POLICY"
